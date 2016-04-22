cea2a053b7fba9cdeb5b4dae38881a04a7c9b3f2
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2016-04-22 23:41:17 +0530

    Shorting the information/comments

--- DataCollectionTypes/NestedList.py
+++ DataCollectionTypes/NestedList.py
@@ -1,21 +1,20 @@
 __author__ = 'Sanjay'
-#
-#
+
 # Input Format
-#
+
 # The first line contains an integer, NN, the number of students.
 # The 2N2N subsequent lines describe each student over 22 lines; the first line contains a student's name, and the second line contains their grade.
-#
+
 # Constraints
 #
 # 2?N?52?N?5
 # There will always be one or more students having the second lowest grade.
 # Output Format
-#
+
 # Print the name(s) of any student(s) having the second lowest grade in Physics; if there are multiple students, order their names alphabetically and print each one on a new line.
-#
+
 # Sample Input
-#
+
 # 5
 # Harry
 # 37.21
@@ -32,9 +31,9 @@ __author__ = 'Sanjay'
 # Berry
 # Harry
 # Explanation
-#
+
 # There are 55 students in this class whose names and grades are assembled to build the following list:
-#
+
 # students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 # The lowest grade of 37.237.2 belongs to Tina. The second lowest grade of 37.2137.21 belongs to both Harry and Berry, so we order their names alphabetically and print each name on a new line.
 
