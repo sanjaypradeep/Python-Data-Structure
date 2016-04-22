dac84477630d3c46f9af63136f0cc9bb4c14d5c2
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2016-04-22 23:38:26 +0530

    Shorting the information/comments

--- itertools/itertools_product.py
+++ itertools/itertools_product.py
@@ -24,46 +24,33 @@ __author__ = 'Sanjay'
 #  and B
 # B
 #  are sorted lists, and the cartesian product's tuples should be output in sorted order.
-#
-#
+
 # Input Format
-#
-#
+
 # The first line contains the space separated elements of list A
 # A
 # .
 #  The second line contains the space separated elements of list B
 # B
 # .
-#
 # Both lists have no duplicate integer elements.
-#
 # Constraints
-#
 # 0<A<30
 # 0<A<30
-#
 # 0<B<30
 # 0<B<30
-#
-#
+
 # Output Format
-#
-#
 # Output the space separated tuples of the cartesian product.
-#
-#
 # Sample Input
-#
+
+
 #  1 2
 #  3 4
-#
-#
-#
+
 # Sample Output
-#
+
 #  (1, 3) (1, 4) (2, 3) (2, 4)
-#
 
 a=map(int,raw_input().split(" "))
 b=map(int,raw_input().split(" "))
