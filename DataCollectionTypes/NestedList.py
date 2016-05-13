4218d31807ebe405524be8ea0a8a3a700c0cd1d6
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2016-05-13 17:25:31 +0530

    Comment Added.

--- DataCollectionTypes/NestedList.py
+++ DataCollectionTypes/NestedList.py
@@ -27,7 +27,7 @@ __author__ = 'Sanjay'
 # Harsh
 # 39
 # Sample Output
-#
+
 # Berry
 # Harry
 # Explanation
@@ -42,3 +42,5 @@ marks = [[raw_input(), float(raw_input())] for i in  xrange(n)]
 scores = sorted({s[1] for s in marks})
 result = sorted(s[0] for s in marks if s[1] == scores[1])
 print '\n'.join(result)
+
+#ends
