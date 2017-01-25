434b969d56a138e7976f8d3dc71abdaf5a9b9f3e
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2017-01-25 23:39:11 +0530

    Added one alternate solution, also code updated to Python 3 Series. All the commented lines are Sample Inputs and Outputs

--- Collections/MostCommon.py
+++ Collections/MostCommon.py
@@ -52,5 +52,11 @@ __author__ = 'Sanjay'
 #  distinct characters.
 
 from collections import Counter
-for k,v in sorted( Counter(raw_input()).most_common(3) , key = lambda x: (-x[1],x[0])):
-         print k,v
+for k,v in sorted( Counter(input()).most_common(3) , key = lambda x: (-x[1],x[0])):
+         print (k,v)
+
+# Solution 2:
+
+userInput = list(str(input()))
+for key, countValue in dict(Counter(userInput)).items():
+    print(key, countValue)
