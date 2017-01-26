4e1104695bb7c065c800b2ee360c39512eea1431
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2017-01-26 20:09:27 +0530

    Implementation of Recurrsion in Python, recursion is not recommended in all cases.

---
+++ Recursion/RecursionTechniq.py
@@ -0,0 +1,36 @@
+__author__ = 'Sanjay'
+
+#calculating a sum of list of numbers.
+
+def sumOfList(inp):
+    s = 0
+    for i in inp:
+        s = s +i
+    print s
+    return s
+
+#Recurssion methodology now
+# three LAWS OF RECURSSION
+# A recursive algorithm must have a base case.
+# A recursive algorithm must change its state and move toward the base case.
+# A recursive algorithm must call itself, recursively.
+
+
+def sumRec(inp):
+    if len(inp) == 1:
+        return 1
+    else:
+        return inp[0] + sumRec(inp[1:])
+
+
+def listsum(numList):
+   if len(numList) == 1:
+        return numList[0]
+   else:
+        return numList[0] + listsum(numList[1:])
+   return numList
+
+if __name__ == '__main__':
+    sumOfList([1,2,3,6,7,8,9,065,6])
+    sumRec([1,90,80,700]) # 3 RECURSSIVE CALLS WIL HAPPEN (TOTAL LENGTH - 1)
+    listsum([1,90,80,700])
---
+++ Recursion/TowersOfHanoi.py
@@ -0,0 +1,16 @@
+__author__ = 'Sanjay'
+
+
+def moveTower(height, fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height-1, fromPole, withPole, toPole)
+        moveDisk(fromPole,toPole)
+        moveTower(height-1,withPole, toPole,fromPole )
+
+
+def moveDisk(fp,tp):
+    print ("moving disk from",fp,"to",tp)
+
+
+if __name__ == '__main__':
+    moveTower(3,"A","B","C")
