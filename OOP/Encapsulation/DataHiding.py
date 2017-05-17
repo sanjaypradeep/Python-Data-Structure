96a5e86f1f8b7095eec7dcbc1c4fb0adf3d7e244
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2017-05-17 23:29:08 +0530

    Comments are corrected andunwatned codes areremoved from the program.

--- OOP/Encapsulation/DataHiding.py
+++ OOP/Encapsulation/DataHiding.py
@@ -12,7 +12,7 @@ class SampleClass(object):
     # constrcutor.
     def __init__(self):
         self.a = 123    # OK to access directly
-        self._a = 456   # should be considered private
+        self._a = 456   # should be considered protected
         self.__a = 789  # considered private, name mangled , more secure Data
 
     def getSecureData(self):
@@ -25,14 +25,6 @@ class SampleClass(object):
     def __getMoreSecureDataMethod(self):
         print(self.__a)
 
-
-
-class ChildClass(SampleClass):
-
-    def __init__(self):
-        super(ChildClass, self).__init__()
-
-
 if __name__ == '__main__':
     # Lets first try to create an object of SampleClass.
     objOfSampleClass = SampleClass() # object will get created here.
@@ -41,9 +33,11 @@ if __name__ == '__main__':
     # just for your reference.
     print(objOfSampleClass.b)
     print(objOfSampleClass._b)
+    # If you want to bring private value/variable outside the class, normally its not possible.
+    # Below is going to throw an error (uncomment and execute it)
     print(objOfSampleClass.__b)
 
-
+    # But if you do still, want to bring the value outside the class. then follow the below line of code.
     # using objOfSampleClass, lets call two global variables
     print(objOfSampleClass.numVariable) # 3691
     print(objOfSampleClass.globalStringVariable) #
@@ -52,10 +46,10 @@ if __name__ == '__main__':
     # lets call those variable values one by one.
     print(objOfSampleClass.a)  # output is 123
     print(objOfSampleClass._a)  # output is 456
-
+    #
     # print(objOfSampleClass.__a)  # uncomment and run this line, it will throw an error.
-
-    # If you want to bring out the private attribute of a class, then please see the next time.
+    #
+    # # If you want to bring out the private attribute of a class, then please see the next time.
     print(objOfSampleClass._SampleClass__a)  # to access __a,
     # object._ClassName__privateVariable
 
@@ -67,17 +61,7 @@ if __name__ == '__main__':
 
     # # Below method has __ [two underscores], which is considered as private method here. So it cannot be called outside the class.
     # Uncomment the below line and test.
-    print("calling a method: ", m.__getMoreSecureData())
+    # print("calling a method: ", m.__getMoreSecureData())
 
     # here we are tyring to get the method out, but it's not possible.
     print("calling a method: ", m._SampleClass__getMoreSecureData())
-
-    print ("inherited here")
-    childObj = ChildClass()
-    print (childObj.a)  # Object of ChildClass class, can call member variable in SampleClass,, because its inherited
-    print (childObj._a)  # ""
-    # print(childObj.__a)     # __a is a private variable which cannot be called outside the class, so it won't work.
-    # to make this happen, please check the next line.
-    print (childObj._C__a)  # can call __ if its inherited and mangeled
-
-    print (childObj.numVariable)  # public, so can be accessed.
