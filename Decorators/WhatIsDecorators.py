ade223f203ef84e381f9b9b2cd1c3ccaaea6ad74
SanjayPradeeo <sanjay.sndk@gmail.com>
2018-09-23 11:55:02 +0530

    What is decorators?
    Defintion and description about decorators with basic example

---
+++ Decorators/WhatIsDecorators.py
@@ -0,0 +1,32 @@
+# One line defition -  A decorator takes in a function, adds some functionality and returns it.
+
+# Description: This is also called metaprogramming as a part of the program tries to modify another part of the program at compile time.
+
+# Prerequisites
+
+# We must be comfortable with the fact that, everything in Python (Yes! Even classes), are objects.
+# Names that we define are simply identifiers bound to these objects. Functions are no exceptions,
+# they are objects too (with attributes). Various different names can be bound to the same function object.
+
+# Lets take the below example
+
+def first(msg):
+    print (msg)
+
+
+first("Hey") #output : Hey
+
+secondVariable = first # assinging first method to secondVariable.
+
+secondVariable("Hello..") #output : Hello
+
+# When you run the code, both functions first and secondVariable gives same output. Here, the names first and secondVariable refer to the same function object.
+#
+# Now things start getting weirder.
+#
+# Functions can be passed as arguments to another function.
+#
+# If you have used functions like map, filter and reduce in Python, then you already know about this.
+
+# Here's the example.
+
