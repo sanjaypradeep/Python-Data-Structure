6591da688006f0e3f0a7e0e009593aa50276ee9e
Sanjay Pradeep Kumar <sanjay_pradeepkumar@symantec.com>
2019-05-25 20:15:34 +0530

    Update WhatIsZip.py
    Further more explanation on zip functioanlity.

--- Zip/WhatIsZip.py
+++ Zip/WhatIsZip.py
@@ -23,3 +23,21 @@ mapped = set(mapped)
 # printing resultant values
 print("The zipped result is : ", end="")
 print(mapped) # The zipped result is : {('Manjeet', 4, 40), ('Shambhavi', 3, 60), ('Nikhil', 1, 50), ('Astha', 2, 70)}
+
+
+#Here's is one other example, 
+
+teachernames = ['Ram', 'Lalitha', 'Gowri']
+subject = ['English', 'Hindi', 'Tamil', 'Math']
+
+# if you see the above two list, there are 3 teachers available inside a list, and there are four subjects
+
+# Here we are trying to map each teacher on each subject, so lets zip it. 
+
+result = zip(subject, teachernames)
+print ("The zipped result is .. ", dict(result))
+
+# output will be - {'English': 'Ram', 'Hindi': 'Lalitha', 'Tamil': 'Gowri'}
+
+# Explanation : from the above result, you can understand there's no teacher is been assinged for subject Map, that's because there're only 3 teachers available right now. 
+# length mismatches.
