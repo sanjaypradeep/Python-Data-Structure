a131c157725073277a54676f5473888b0e2fec56
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2019-07-03 23:18:42 +0530

    Update BubbleSort.py
    New implementation fo Bubble Search and tested,

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -8,7 +8,17 @@ def bubbleSort(alist):
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
 
+def bS(givenList):
+    givListLen = len(givenList) - 1
+    for i in range(givListLen):
+        for j in range(givListLen - i):
+            if givenList[j] > givenList[j+1]:
+                givenList[j], givenList[j+1] = givenList[j+1], givenList[j]
+    return givenList
+
 if __name__ == '__main__':
     alist = [54,26,93,17,77,31,44,55,20]
     bubbleSort(alist)
     print(alist)
+
+    print(bS(alist))
