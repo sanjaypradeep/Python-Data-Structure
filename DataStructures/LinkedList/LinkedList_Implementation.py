043e3a7828072911f7094873bdbc51c64d119f6f
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2019-09-11 23:00:05 +0530

    Desc addition.

--- DataStructures/LinkedList/LinkedList_Implementation.py
+++ DataStructures/LinkedList/LinkedList_Implementation.py
@@ -4,6 +4,8 @@
 #Description : A linked list is a linear data structure, in which the elements are not stored at contiguous
 #              memory locations. The elements in a linked list are linked using pointers.
 
+# In simple words, a linked list consists of nodes where each node contains a data field
+# and a reference(link) to the next node in the list.
 
 class node:
     def __init__(self, data=None, next=None):
