9db4d2ff31bdc1ac1bbb96ec151ff5e233049761
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2020-02-19 02:13:48 +0530

    Removing hardcoded date.
    not using hardcoded date and instead by using default library, constructing date format and processing.

--- Excersise/GoldRate/HelloWorld.py
+++ Excersise/GoldRate/HelloWorld.py
@@ -17,9 +17,10 @@ def setGoldPrice(K, g, price):
 def getDateFormat():
     import datetime
     todayDateInfo = datetime.datetime.now()   
-    output= str(todayDateInfo.day) +'/' + str(todayDateInfo.strftime("%B")) + "/" + str(todayDateInfo.year) 
-    print(output)
-    return output
+    return str(todayDateInfo.day) +'/' + str(todayDateInfo.strftime("%B")) + "/" + str(todayDateInfo.year)         
+
+def getIndexValueOfTodaysDate():
+    return finalList.index(getDateFormat()+'\n')
 
 import requests 
 from bs4 import BeautifulSoup 
@@ -31,18 +32,19 @@ soup = BeautifulSoup(r.content, 'html5lib')
 
 table = soup.find('table', attrs = {'class':'table-price'}) 
 
-tempData = table.get_text()
+#OverAll table date is assinged to the below variable. 
+overAllTableData = table.get_text()
 
+#Separating each table text and assigning into list.
+allData = list(overAllTableData.split(" "))
 
-allData = list(tempData.split(" "))
-
+# remove all empty string elements inside the list.
 listOfText = [i for i in allData if i]
 
+# removes all independent \n elements inside the list.
 finalList = [i for i in listOfText if i != '\n']
-#print(test_list)
 
-indexValue = finalList.index(getDateFormat()+'\n')
-# print(indexValue)
+indexValue = getIndexValueOfTodaysDate()
 
 setGoldPrice('22K', '1g', finalList[indexValue+3])
 setGoldPrice('22K' , '8g', finalList[indexValue+4])
@@ -51,11 +53,9 @@ setGoldPrice('24K', '1g', finalList[indexValue+1])
 setGoldPrice('24K', '8g', finalList[indexValue+2])
 
 print("24K Gold - 1 Gram price is " + gold_rate['24K']['1g'])
-
 print("24K Gold - 8 Gram price is " + gold_rate['24K']['8g'])
 
 print("22K Gold - 1 Gram price is " + gold_rate['22K']['1g'])
-
 print("22K Gold - 8 Gram price is " + gold_rate['22K']['8g'])
 
 import time
