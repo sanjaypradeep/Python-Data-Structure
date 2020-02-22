aabc327e173cc523cf68ba406711632df74e6b9f
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2020-02-22 16:47:59 +0530

    Initiated storing the data to my local database

--- Excersise/GoldRate/HelloWorld.py
+++ Excersise/GoldRate/HelloWorld.py
@@ -11,19 +11,11 @@ gold_rate = {
     }
 }
 
-def setGoldPrice(K, g, price):
-    gold_rate[K][g] = price 
-
-def getDateFormat():
-    import datetime
-    todayDateInfo = datetime.datetime.now()   
-    return str(todayDateInfo.day) +'/' + str(todayDateInfo.strftime("%B")) + "/" + str(todayDateInfo.year)         
-
-def getIndexValueOfTodaysDate():
-    return finalList.index(getDateFormat()+'\n')
 
 import requests 
+from datetime import date
 from bs4 import BeautifulSoup 
+
   
 URL = "https://www.livechennai.com/gold_silverrate.asp"
 r = requests.get(URL) 
@@ -32,31 +24,117 @@ soup = BeautifulSoup(r.content, 'html5lib')
 
 table = soup.find('table', attrs = {'class':'table-price'}) 
 
-#OverAll table date is assinged to the below variable. 
-overAllTableData = table.get_text()
+tempData = table.get_text()
 
-#Separating each table text and assigning into list.
-allData = list(overAllTableData.split(" "))
 
-# remove all empty string elements inside the list.
-listOfText = [i for i in allData if i]
+allData = list(tempData.split(" "))
 
-# removes all independent \n elements inside the list.
-finalList = [i for i in listOfText if i != '\n']
 
-indexValue = getIndexValueOfTodaysDate()
+test_list1 = [i for i in allData if i]
 
-setGoldPrice('22K', '1g', finalList[indexValue+3])
-setGoldPrice('22K' , '8g', finalList[indexValue+4])
+test_list = [i for i in test_list1 if i != '\n']
 
-setGoldPrice('24K', '1g', finalList[indexValue+1])
-setGoldPrice('24K', '8g', finalList[indexValue+2])
+todayDate = "11/February/2020\n"
+
+dateObj = date.today()
+
+todayDate = dateObj.strftime("%d") + "/" + dateObj.strftime("%B") + "/" + dateObj.strftime("%Y") + "\n" 
+todaysDateFormatToDB = dateObj.strftime("%Y")+'-'+dateObj.strftime("%m")+'-'+dateObj.strftime("%d")
+
+try:
+    indexValue = test_list.index(todayDate)
+except Exception as ex:
+    print("Error Info: " + ex)
+
+# goldRate24_1gram = test_list[indexValue+1]
+
+gold_rate['24K']['1g'] =  test_list[indexValue+1]
+gold_rate['24K']['8g'] =  test_list[indexValue+2]
+gold_rate['22K']['1g'] =  test_list[indexValue+3]
+gold_rate['22K']['8g'] =  test_list[indexValue+4]
 
 print("24K Gold - 1 Gram price is " + gold_rate['24K']['1g'])
+
 print("24K Gold - 8 Gram price is " + gold_rate['24K']['8g'])
 
 print("22K Gold - 1 Gram price is " + gold_rate['22K']['1g'])
+
 print("22K Gold - 8 Gram price is " + gold_rate['22K']['8g'])
 
 import time
-time.sleep(15)
+#time.sleep(25)
+
+import mysql.connector
+from mysql.connector import Error
+
+def connectToDB():
+    try:
+
+        connection = mysql.connector.connect(host='localhost',
+                                            database='world',
+                                            user='root',
+                                            password='root')
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            print("Connected to MySQL Server version ", db_Info)
+            cursor = connection.cursor()
+            cursor.execute("select database();")
+            record = cursor.fetchone()
+            print("You're connected to database: ", record)
+            return connection
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+
+
+def insertRecord(con):
+    if con.is_connected():
+        # print("=======================================")
+        # print(todaysDateFormatToDB)
+        # print(gold_rate['24K']['8g'])
+        # print(gold_rate['24K']['1g'])
+        # print(gold_rate['22K']['8g'])
+        # print(gold_rate['22K']['1g'])
+
+        # sample = []
+        # sample.insert(0, todaysDateFormatToDB)
+        # sample.insert(1, gold_rate['24K']['8g'])
+        # sample.insert(2, gold_rate['24K']['1g'])
+        # sample.insert(3, gold_rate['22K']['8g'])
+        # sample.insert(4, gold_rate['22K']['1g'])
+
+        # print("finally...." , sample)
+        # print("=======================================")
+
+        mySql_insert_query = """INSERT INTO `world`.`daily_gold_rates` (`todays_date`, 
+													`twentyFour_carot_eight_grams`, 
+													`twentyFour_carot_one_gram`, 
+													`twentyTwo_carot_eight_grams`, 
+													`twentyTwo_carot_one_gram` ) 
+                                VALUES ('2019-08-14', '33455', '4252', '31253', '4072');"""
+
+        something = "INSERT INTO `world`.`daily_gold_rates` (`todays_date`, `twentyFour_carot_eight_grams`,`twentyFour_carot_one_gram`, `twentyTwo_carot_eight_grams`, `twentyTwo_carot_one_gram` ) VALUES ('" + str(todaysDateFormatToDB) +"','"+ gold_rate['24K']['8g'] +"'," + "'"+gold_rate['24K']['1g']+ "'," + "'"+ gold_rate['22K']['8g'] +"',"+ "'" + gold_rate['22K']['1g'] +"'" +");" 
+
+        # print(something)
+        # time.sleep(10)
+
+        # something2 = "INSERT INTO `world`.`daily_gold_rates` (`todays_date`, `twentyFour_carot_eight_grams`,`twentyFour_carot_one_gram`,`twentyTwo_carot_eight_grams`,`twentyTwo_carot_one_gram`  ) VALUES (" +str(sample[0]) +',' + sample[1] +',' + sample[2]+','+ sample[3]+','+sample[4]+');'
+
+        # print("something2.. ", something2)
+        # time.sleep(10)
+
+        # something = "INSERT INTO `world`.`daily_gold_rates` (`todays_date`, `twentyFour_carot_eight_grams`, `twentyFour_carot_one_gram`, `twentyTwo_carot_eight_grams`, `twentyTwo_carot_one_gram` ) VALUES (" + todaysDateFormatToDB +");" 
+
+        cursor = con.cursor()
+        cursor.execute(something)
+        con.commit()
+        print(cursor.rowcount, "Record inserted successfully into Laptop table")
+        # con.close()
+
+def disconnectDB(con):
+        if (con.is_connected()):
+            con.close()
+            print("MySQL connection is closed")
+
+connectionHook = connectToDB()
+insertData = insertRecord(connectionHook)
+diconnect = disconnectDB(connectionHook)
