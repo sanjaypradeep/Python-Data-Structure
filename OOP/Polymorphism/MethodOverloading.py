053f26bcf8fa010bc989c7c5735b31d958a39103
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2020-06-24 11:18:48 +0530

    Re-visited this topic - naming convention and made more readable

--- OOP/Polymorphism/MethodOverloading.py
+++ OOP/Polymorphism/MethodOverloading.py
@@ -1,17 +1,32 @@
 # author = _'Sanjay'
 
-class A(object):
+class SampleClass(object):
 
+    # for name sake, I've defined a default constructor.
     def __init__(self):
         pass
+        
+    def add(self, firstNum, secondNum):
+        '''
+        @name: add
+        @param: firstNum, secondNum
+        @desc: Takes two input paramter, adding them and returning
+        @return: sum(firstNum, secondNum)
+        '''
+        return self.firstNum + self.secondNum
 
-    def add(self, a, b):
-        print self.a + self.b
-
-    def add(self, a, b, c):
-        print a + b + c
+    
+    # this method does the same as above, but the difference is, it takes 3 parameter here.
+    def add(self, firstNum, secondNum, thirdNum):
+        '''
+        @name : add
+        @param: firstNum, secondNum, thirdNum
+        @desc: takes three input parameter, adding them and returning
+        @return : sum(firstNum, secondNum, threeNum)
+        '''
+        return self.firstNum + self.secondNum + self.thirdNum
 
 #Method overloading is not possible
 # This is compile time polymorphism. 
-x = A()
+x = SampleClass()
 x.add(1,2,3)
