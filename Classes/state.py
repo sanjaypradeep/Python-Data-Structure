f9ddff652dd2aa9263e3224630cec35d1c2acd4f
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2020-06-27 18:53:56 +0530

    More infor added

--- Classes/state.py
+++ Classes/state.py
@@ -23,7 +23,39 @@ def withdraw(amount):
 sanjay = deposit(100)
 print(sanjay)
 
-# The above example is good enough only if we want to have just a single account. 
+# The above example is good enough only if we want to have just a single account. Did you understand what I said?
+
 
 # ******Things start getting complicated if want to model multiple accounts.******
+
+# We can solve the problem by making the state local, probably by using a dictionary to store the state.
+
+def make_account():
+    return {'balance': 0}
+
+def putDeposit(account, amount):
+    account['balance'] += amount
+    return account['balance']
+
+def withdrawMoney(account, amount):
+    account['balance'] -= amount
+    return account['balance']
+
+def checkBalance(account):
+    return account['balance']
+
+
+sanjay = make_account()
+
+ram = make_account()
+
+# by default, when someone creates an account, balance will show 0.
+putDeposit(sanjay, 5000)
+
+# what is sanjay's balance amount now?
+print("Sanjay has " + str(checkBalance(sanjay)) + " in his account!")
+
+print("Sanjay has withdrawn 2500 rs from the account, therefore the remaining balance is " + str(withdrawMoney(sanjay, 2500)))
+
+
 
