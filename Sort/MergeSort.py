0cb151d0178f71eedb9f97a7d2a75bfe1db38499
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2020-10-20 19:26:30 +0530

    Addition of sorting algorithms and it's implementation.

---
+++ Sort/MergeSort.py
@@ -0,0 +1,43 @@
+__author__ = 'Sanjay'
+
+# Merge sort is a very efficient sorting algorithm. 
+# It’s based on the divide-and-conquer approach, a powerful algorithmic technique used to solve complex problems.
+
+def mergeSort(alist):
+    print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+
+if __name__ == '__main__':
+
+    alist = [54,26,93,17,77,31,44,55,20]
+    mergeSort(alist)
+    print(alist)
