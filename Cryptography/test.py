a09602fbf2df7a276a2e60a40aaf955e7f905028
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2025-07-23 14:10:23 -0400

    improvised the lines of code - bringing to standard.

--- Cryptography/test.py
+++ Cryptography/test.py
@@ -1,24 +1,56 @@
-# Basic Fernet
+# Enhanced Fernet Encryption Example
 
-# msg = "I am going to encrypt this sentence using Fernet"
-msg = "D$bd1@k3"
-
-from cryptography import fernet
 from cryptography.fernet import Fernet
 
-# first generate key
-eKey = Fernet.generate_key()
-print("\nKey is : " + eKey.decode())
-# print("\nType of key is .. " + str(type(eKey)))
-print("\nKey is : " + str(len(eKey.decode())))
+def generate_key():
+    """Generate and return a Fernet key."""
+    return Fernet.generate_key()
 
-# create a class instance object
-f1 = Fernet(eKey)
+def save_key(key, filename):
+    """Save the key to a file."""
+    with open(filename, 'wb') as file:
+        file.write(key)
 
-eMsg = f1.encrypt(msg.encode()) # f1 instance can encrypt only bytes
-print("\nEncrypted message is - " + eMsg.decode())
-print("\nEncrypted message is -" + str(len(eMsg)))
+def load_key(filename):
+    """Load the key from a file."""
+    with open(filename, 'rb') as file:
+        return file.read()
 
-# decrypt with same instance
-dMsg = f1.decrypt(eMsg).decode()
-print("\nDecrypted Message - " + dMsg)
+def encrypt_message(message, key):
+    """Encrypt a message using the provided key."""
+    f = Fernet(key)
+    return f.encrypt(message.encode())
+
+def decrypt_message(token, key):
+    """Decrypt a token using the provided key."""
+    f = Fernet(key)
+    return f.decrypt(token).decode()
+
+if __name__ == "__main__":
+    msg = "D$bd1@k3"
+    key_file = "fernet.key"
+
+    # Generate and save key
+    key = generate_key()
+    print("\nGenerated Key:", key.decode())
+    save_key(key, key_file)
+    print("Key saved to", key_file)
+
+    # Load key (simulating a new session)
+    loaded_key = load_key(key_file)
+    print("Loaded Key:", loaded_key.decode())
+
+    # Encrypt message
+    try:
+        encrypted = encrypt_message(msg, loaded_key)
+        print("\nEncrypted Message:", encrypted.decode())
+        print("Encrypted message length:", len(encrypted))
+    except Exception as e:
+        print("Encryption error:", e)
+
+    # Decrypt message
+    try:
+        decrypted = decrypt_message(encrypted, loaded_key)
+        print("\nDecrypted Message:", decrypted)
+    except Exception as e:
+        print("Decryption error:", e)
